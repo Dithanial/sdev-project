f904a44c1d4b62fb5a50d7360d9b4a3f5941682f
Stephen Smoogen <stephen.smoogen@mymail.champlain.edu>
2016-12-04 16:18:29 -0500

    put in the items from Demostenes

--- TicketMaster.h
+++ TicketMaster.h
@@ -15,279 +15,353 @@ const char SeatAvail = '#';
 const char SeatTaken = '*';
 
 class TicketMaster {
- private:
+private:
 
-  int seatsSold;
-  float totalMoney;
+	int seatsSold=0;
+	float totalMoney=0;
 
-  struct SeatStructures {
-    char  seatstatus;
-    float seatprice;
-    SeatStructures(char sold=SeatAvail, float price = 0.0){
-      seatstatus = sold;
-      seatprice = price;
-    };
-  };
-  SeatStructures auditorium[Max_Rows][Max_Cols];
+	struct SeatStructures {
+		char  seatstatus;
+		float seatprice;
+		SeatStructures(char sold = SeatAvail, float price = 0.0) {
+			seatstatus = sold;
+			seatprice = price;
+		};
+	};
+	SeatStructures auditorium[Max_Rows][Max_Cols];
 
- public:
-  TicketMaster();
-  ~TicketMaster();
-  
-  // a getter for seat availability 
-  bool isSeatSold(int row, int col){
-    if (auditorium[row][col].seatstatus == SeatAvail){
-      return true;
-    }
-    else
-      return false;
-  }
+public:
+	TicketMaster();
+	~TicketMaster();
 
-  // a bounds checker
-  bool insideSS(int ,int ); //defined below
+	// a getter for seat availability 
+	bool isSeatSold(int row, int col) {
+		if (auditorium[row][col].seatstatus == SeatAvail) {
+			return true;
+		}
+		else
+			return false;
+	}
 
-  // get the price of the seat
-  float getSeatPrice(int row){
-    return auditorium[row][0].seatprice;
-  }
+	// a bounds checker
+	bool insideSS(int, int); //defined below
 
-  void setSeatPrice(int row, int col,float price){
-    auditorium[row][col].seatprice = price;
-  }
+							 // get the price of the seat
+	float getSeatPrice(int row) {
+		return auditorium[row][0].seatprice;
+	}
 
-  void markSold(int row, int col){
-    auditorium[row][col].seatstatus = SeatTaken;
-  }
+	void setSeatPrice(int row, int col, float price) {
+		auditorium[row][col].seatprice = price;
+	}
 
-  char getSeat(int row, int col){
-    return auditorium[row][col].seatstatus;
-  }
-  
-  void displaySeats();
+	void markSold(int row, int col) {
+		auditorium[row][col].seatstatus = SeatTaken;
+	}
 
-  void clearSeats();
+	char getSeat(int row, int col) {
+		return auditorium[row][col].seatstatus;
+	}
 
-  int requestTickets(int, int, int);
-  
-  string purchaseTickets(int, int, int, float);
-  
-  void salesReport() {
-    cout << "salesReport\n";
-    cout << "Seats sold: " << getSeatsSold() << " Total Money: " << getTotalMoney() << endl;
-  }
+	void displaySeats();
 
-  int getSeatsSold() {
-    return seatsSold;
-  }
+	void clearSeats();
 
-  void setSeatsSold(int sold) {
-    seatsSold += sold;
-  }
+	int requestTickets(int, int, int);
 
-  void clearSeatsSold(){
-    seatsSold = 0;
-  }
+	string purchaseTickets(int, int, int, float);
 
-  float getTotalMoney() {
-    return totalMoney;
-  }
+	void salesReport() {// print a sales report 
 
-  void setTotalMoney(float sold) {
-    totalMoney += sold;
-  }
+		int seatsLeft; //variable declaration
+		seatsLeft = (Max_Rows * Max_Cols - getSeatsSold());// store in the seatsLeft variable the empty seats
 
-  void clearTotalMoney() {
-    totalMoney = 0.0;
-  }
-	
+		//Print report 
+		cout << "\n\t\t\t   Sales Report";
+		cout<<"\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n   ";
+		cout << "Seats sold: " << getSeatsSold() << "      Total Money: $" << getTotalMoney() << "\t SeatsLeft:  " << seatsLeft << endl;
+		cout << "* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n:   ";
+	}
+
+	int getSeatsSold() {
+		return seatsSold;
+	}
+
+	void setSeatsSold(int sold) {
+		seatsSold += sold;
+	}
+
+	void clearSeatsSold() {
+		seatsSold = 0;
+	}
+
+	float getTotalMoney() {
+		return totalMoney;
+	}
+
+	void setTotalMoney(float sold) {
+		totalMoney += sold;
+	}
+
+	void clearTotalMoney() {
+		totalMoney = 0.0;
+	}
+	void reportManagment();
+
 };
 
 TicketMaster::TicketMaster() {
 
-  // Read the price file and fill in data.
-  ifstream PriceFile;
-  ifstream SeatsFile;
+	// Read the price file and fill in data.
+	ifstream PriceFile;
+	ifstream SeatsFile;
+	ifstream ticketSoldFile;// file containing the ticket sales
+	ifstream moneyFile;// file containing the total sales
 
-  // Set up the prices for each row of seats.
-  float tempPrice = 0.0;
-  string tempData ="";
-  int row = 0;
-  int col = 0;
-  PriceFile.open("SeatPrices.dat");
-  if (!PriceFile)
-    cout << "Not able to read price file";
-  else {
-    while ((row < Max_Rows) && (PriceFile >> tempPrice)){
-      for (col=0; col< Max_Cols; col++){
-        setSeatPrice(row,col,tempPrice);
-      }
-      row++;
-    }
-  };
-  PriceFile.close();
+	// Set up the prices for each row of seats.
+	float tempPrice = 0.0;
+	string tempData = "";
+	string displayTempData;
+	int row = 0;
+	int col = 0;
+	PriceFile.open("SeatPrices.dat");
+	if (!PriceFile)
+		cout << "Not able to read price file";
+	else {
+		while ((row < Max_Rows) && (PriceFile >> tempPrice)) {
+			for (col = 0; col< Max_Cols; col++) {
+				setSeatPrice(row, col, tempPrice);
+			}
+			row++;
+		}
+	};
+	PriceFile.close();
 
-  // Set up the taken seats in auditorium
-  row = 0;
-  SeatsFile.open("SeatAvailability.dat");
-  if (!SeatsFile)
-    cout << "Not able to read available seats file";
-  else {
-    while ((row < Max_Rows) && (SeatsFile >> tempData)){
-      col = 0;
-      while (col < Max_Cols){
-      	if (char(tempData[col]) == SeatTaken) {
-      	  markSold(row,col);
-      	}
-      	col++;
-      };
-      row++;
-    }
-  }
-  SeatsFile.close();
+	// Set up the taken seats in auditorium
+	row = 0;
+	SeatsFile.open("SeatAvailability.dat");
+	if (!SeatsFile)
+		cout << "Not able to read available seats file";
+	else {
+		while ((row < Max_Rows) && (SeatsFile >> tempData)) {
+			col = 0;
+			while (col < Max_Cols) {
+				if (char(tempData[col]) == SeatTaken) {
+					markSold(row, col);
+				}
+				col++;
+			};
+			row++;
+		}
+	}
+	SeatsFile.close();
+
+	
+// copying from seatReport.dat  into variable seatSold
+	ticketSoldFile.open("seatReport.dat");
+	if (!ticketSoldFile)
+		cout << "Not able to read price file";
+	else {
+		
+		ticketSoldFile >> seatsSold;
+	}
+	ticketSoldFile.close();
+// copying from moneyReport.dat into variable totalMoney
+	moneyFile.open("moneyReport.dat");
+	if (!moneyFile)
+		cout << "Not able to read price file";
+	else {
+
+		moneyFile >> totalMoney;
+	}
+	moneyFile.close();
 }
 
 // Bounds checking
 bool TicketMaster::insideSS(int row, int col) {
-  if ((row < 0) || (row >= Max_Rows)) {return false;};
-  if ((col < 0) || (row >= Max_Cols)) {return false;};
-  return true;
+	if ((row < 0) || (row >= Max_Rows)) { return false; };
+	if ((col < 0) || (row >= Max_Cols)) { return false; };
+	return true;
 }
 
 // Display seating chart
 void TicketMaster::displaySeats() {
-  cout << "     Seats" << endl;
-  int numX = Max_Cols / 10; // work out the number of rows
-  int remX = Max_Cols % 10; // work out the left over
-  cout << "   ";
-  for (int c = 0; c < numX; c++){
-    for (int i = 1; i < 10; i++){
-      cout << i;
-    };
-    cout << "0";
-  }
-  for (int i=1; i<=remX; i++){
-    cout << i;
-  };
-  cout << endl;
-  // print the rows.
-  for (int r = 0; r < Max_Rows; r++){
-    cout << setw(2)  << (r+1) << " ";
-    for (int c = 0; c < Max_Cols; c++){
-      cout << getSeat(r,c);
-    };
-    cout << endl;
-  };
-  cout << endl;
+	cout << "     Seats" << endl;
+	int numX = Max_Cols / 10; // work out the number of rows
+	int remX = Max_Cols % 10; // work out the left over
+	cout << "   ";
+	for (int c = 0; c < numX; c++) {
+		for (int i = 1; i < 10; i++) {
+			cout << i;
+		};
+		cout << "0";
+	}
+	for (int i = 1; i <= remX; i++) {
+		cout << i;
+	};
+	cout << endl;
+	// print the rows.
+	for (int r = 0; r < Max_Rows; r++) {
+		cout << setw(2) << (r + 1) << " ";
+		for (int c = 0; c < Max_Cols; c++) {
+			cout << getSeat(r, c);
+		};
+		cout << endl;
+	};
+	cout << endl;
 }
 
 // Check if tickets are available
 int TicketMaster::requestTickets(int seats, int row, int start) {
 
-  for (int i = start; i < start + seats; i++) {
-    if (!isSeatSold(row, i)) {
-      return 1;
-    }
-  }
-return 0;
+	for (int i = start; i < start + seats; i++) {
+		if (!isSeatSold(row, i)) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 // purchase tickets
 string TicketMaster::purchaseTickets(int seats, int row, int start, float price) {
-  float payment = 0.0;
-  char retry = ' ';
+	float payment = 0.0;
+	char retry = ' ';
 
-  // Validate and accept money input
-  while (true) {
-    cout << "How much will you be paying with? $";
-    cin >> payment;
-    if (payment < price) {
-      while (true) {
-        cout << "Not enough funds to continue the transaction. Would you like to try again? (Y or N) ";
-        cin >> retry;
-        if (retry != 'y' && retry != 'Y' && retry != 'n' && retry != 'N') {
-          cout << "Invalid entry, please choose Y or N.\n";
-        } else if (retry == 'N' || retry == 'n')
-            return "Transaction Canceled.\n\n";
-          else 
-            break;
-      }
-    } else {
-        cout << "You will get $" << fixed << setprecision(2) << payment - price << " in change. Here are your tickets.\n";
-        break;
-      }
-  }
+	// Validate and accept money input
+	while (true) {
+		cout << "How much will you be paying with? $";
+		cin >> payment;
+		if (payment < price) {
+			while (true) {
+				cout << "Not enough funds to continue the transaction. Would you like to try again? (Y or N) ";
+				cin >> retry;
+				if (retry != 'y' && retry != 'Y' && retry != 'n' && retry != 'N') {
+					cout << "Invalid entry, please choose Y or N.\n";
+				}
+				else if (retry == 'N' || retry == 'n')
+					return "Transaction Canceled.\n\n";
+				else
+					break;
+			}
+		}
+		else {
+			cout << "You will get $" << fixed << setprecision(2) << payment - price << " in change. Here are your tickets.\n";
+			break;
+		}
+	}
 
-  // Mark seats as sold
-  for (int i = start; i < seats + start; i++) {
-    markSold(row, i);
-  }
-  setSeatsSold(seats);
-  setTotalMoney(price);
+	// Mark seats as sold
+	for (int i = start; i < seats + start; i++) {
+		markSold(row, i);
+	}
+	setSeatsSold(seats);
+	setTotalMoney(price);
 
-  // String manipulation for return purposes
-  string tickets;
-  stringstream ssPrice, ssTicket, ssRow, ssStart;
-  ssPrice << fixed;
-  ssPrice.precision(2);
+	// String manipulation for return purposes
+	string tickets;
+	stringstream ssPrice, ssTicket, ssRow, ssStart;
+	ssPrice << fixed;
+	ssPrice.precision(2);
 
-  // Print Tickets.
-  for (int i = 1; i <= seats; i++) {
-    tickets += "Ticket ";
-    ssTicket.str("");
-    ssTicket << i;
-    tickets += ssTicket.str();;
+	// Print Tickets.
+	for (int i = 1; i <= seats; i++) {
+		tickets += "Ticket ";
+		ssTicket.str("");
+		ssTicket << i;
+		tickets += ssTicket.str();;
 
-    tickets += "\n* * * * * * * * * * * * * * * * * * * * * * *\nRow:   ";
-    ssRow.str("");
-    ssRow << row + 1;
-    tickets += ssRow.str();
+		tickets += "\n* * * * * * * * * * * * * * * * * * * * * * *\nRow:   ";
+		ssRow.str("");
+		ssRow << row + 1;
+		tickets += ssRow.str();
 
-    tickets += "\tSeat:   ";
-    ssStart.str("");
-    ssStart << start + i;
-    tickets += ssStart.str();
+		tickets += "\tSeat:   ";
+		ssStart.str("");
+		ssStart << start + i;
+		tickets += ssStart.str();
 
-    tickets += "\tPrice: $";
-    ssPrice.str("");
-    ssPrice << (price / seats);
-    tickets += ssPrice.str();
+		tickets += "\tPrice: $";
+		ssPrice.str("");
+		ssPrice << (price / seats);
+		tickets += ssPrice.str();
 
-    tickets += "\n* * * * * * * * * * * * * * * * * * * * * * *\n\n";
-  }
+		tickets += "\n* * * * * * * * * * * * * * * * * * * * * * *\n\n";
+	}
 
-  return tickets;
+	return tickets;
 }
 
 
 TicketMaster::~TicketMaster() {
-  string tempData = "";
-  ofstream SeatsFile;
+	string tempData = "";
+	ofstream SeatsFile;
+	ofstream ticketSoldFile;
+	ofstream moneyFile;
 
-  SeatsFile.open("SeatAvailability.dat");
-  if (!SeatsFile)
-    cout << "Not able to read available seats file.\n";
-  else {
-    for (int row = 0; row < Max_Rows; row++) {
-      for (int col = 0; col < Max_Cols; col++) {
-        SeatsFile << getSeat(row, col);
-        //cout << getSeat(row, col);
-      }
-      SeatsFile << endl;
-      //cout << endl;
-    }
-  }
-  SeatsFile.close();
+	SeatsFile.open("SeatAvailability.dat");
+	if (!SeatsFile)
+		cout << "Not able to read available seats file.\n";
+	else {
+		for (int row = 0; row < Max_Rows; row++) {
+			for (int col = 0; col < Max_Cols; col++) {
+				SeatsFile << getSeat(row, col);
+				//cout << getSeat(row, col);
+			}
+			SeatsFile << endl;
+			//cout << endl;
+		}
+	}
+	SeatsFile.close();
+
+//Updating the information of sold tickets
+	ticketSoldFile.open("seatReport.dat");
+	if (!ticketSoldFile)
+		cout << "Not able to read price file";
+	else {
+
+		ticketSoldFile << seatsSold;
+	}
+	ticketSoldFile.close();
+//Updating the information of the money from sells
+	moneyFile.open("moneyReport.dat");
+	if (!moneyFile)
+		cout << "Not able to read price file";
+	else {
+
+		moneyFile << totalMoney;
+	}
+	moneyFile.close();
 }
+
+
 
 void TicketMaster::clearSeats() {
 
-  for (int row = 0; row < Max_Rows; row++) {
-    for (int col = 0; col < Max_Cols; col++) {
-      auditorium[row][col].seatstatus = SeatAvail;
-    }
-  }
-  clearSeatsSold();
-  clearTotalMoney();
+	for (int row = 0; row < Max_Rows; row++) {
+		for (int col = 0; col < Max_Cols; col++) {
+			auditorium[row][col].seatstatus = SeatAvail;
+		}
+	}
+	clearSeatsSold();
+	clearTotalMoney();
 }
+
+/*void TicketMaster::reportManagment() {
+
+	// Read the price file and fill in data.
+	ofstream ticketSoldFile;
+	// Set up the prices for each row of seats.
+	int soldSeats = 0;
+
+	ticketSoldFile.open("SalesReport.dat");
+	if (!ticketSoldFile)
+		cout << "Not able to read price file";
+	else {
+		ticketSoldFile << getSeatsSold() << endl;
+		
+	}
+	ticketSoldFile.close();
+
+}*/
 
 #endif
--- project.cpp
+++ project.cpp
@@ -1,14 +1,18 @@
+// project.cpp : Defines the entry point for the console application.
+//
 
-
+#include "stdafx.h"
+#include "TicketMaster.h"
 #include <iostream>
 #include <iomanip>
-#include "TicketMaster.h"
+#include <string>
 
 using namespace std;
 
 // Prototypes
 void requestSelected(TicketMaster&);
 void purchase(TicketMaster&, int, int, int);
+
 
 int main() {
 
@@ -22,21 +26,23 @@ int main() {
 
 			if (choice > 0 && choice <= 5)
 				break;
-			else 
+			else
 				cout << "Invalid entry. Please try again.\n\n";
 		}
 
-	
+
 		switch (choice) {
-			case 1: tm.displaySeats();
-							break;
-			case 2: requestSelected(tm);
-							break;
-			case 3: tm.salesReport();
-							break;
-			case 4: cout << "Goodbye!\n";
-							break;
-			case 5: tm.clearSeats();
+		case 1: tm.displaySeats();
+			break;
+		case 2: requestSelected(tm);
+			break;
+		case 3: tm.salesReport();
+			
+			
+			break;
+		case 4: cout << "Goodbye!\n";
+			break;
+		case 5: tm.clearSeats();
 		}
 	} while (choice != 4);
 
@@ -44,57 +50,58 @@ int main() {
 }
 
 void requestSelected(TicketMaster &tm) {
-  int reqSeats = 0, reqRow = 0, reqStart = 0, retry = 0;
+	int reqSeats = 0, reqRow = 0, reqStart = 0, retry = 0;
 
-  // Get the Requested number of seats
-  while (true) {
-	cout << "How many seats would you like (30 max)? ";
-    cin >> reqSeats;
-    if (reqSeats > 0 && reqSeats <= 30)
-      break;
-    else
-       cout << "Invalid number, please try again.\n";
-  }
+	// Get the Requested number of seats
+	while (true) {
+		cout << "How many seats would you like (30 max)? ";
+		cin >> reqSeats;
+		if (reqSeats > 0 && reqSeats <= 30)
+			break;
+		else
+			cout << "Invalid number, please try again.\n";
+	}
 
-  // Get the Requested Row
-  while (true) {
-  cout << "Which Row would you like (1-15)? ";
-    cin >> reqRow;
-    if (reqRow > 0 && reqRow <= 15)
-      break;
-    else
-       cout << "Invalid number, please try again.\n";
-  }
+	// Get the Requested Row
+	while (true) {
+		cout << "Which Row would you like (1-15)? ";
+		cin >> reqRow;
+		if (reqRow > 0 && reqRow <= 15)
+			break;
+		else
+			cout << "Invalid number, please try again.\n";
+	}
 
-  // Get the Requested Starting Seat
-  while (true) {
-  	bool good = false, enough = false;
-	  cout << "Which seat would you like to start with (1-" << 31 - reqSeats <<")? ";
-	    cin >> reqStart;
-    if (reqStart > 0 && reqStart <= 30)
-      good = true;
-    else
-       cout << "Invalid number, please try again.\n";
-    if (reqSeats + reqStart > 31){
-    	cout << "Not enough seats in a row for for you to have " <<
-    		reqSeats << " seats starting at " << reqStart << ". Please " <<
-    		"select a lower starting seat number.\n";
-    } else {
-    		enough = true;
-    }
-    if (good && enough)
-    	break;
-  }
+	// Get the Requested Starting Seat
+	while (true) {
+		bool good = false, enough = false;
+		cout << "Which seat would you like to start with (1-" << 31 - reqSeats << ")? ";
+		cin >> reqStart;
+		if (reqStart > 0 && reqStart <= 30)
+			good = true;
+		else
+			cout << "Invalid number, please try again.\n";
+		if (reqSeats + reqStart > 31) {
+			cout << "Not enough seats in a row for for you to have " <<
+				reqSeats << " seats starting at " << reqStart << ". Please " <<
+				"select a lower starting seat number.\n";
+		}
+		else {
+			enough = true;
+		}
+		if (good && enough)
+			break;
+	}
 
-  reqRow--;
-  reqStart--;
+	reqRow--;
+	reqStart--;
 
-  switch (tm.requestTickets(reqSeats, reqRow, reqStart)) {
-  	case 0: purchase(tm, reqSeats, reqRow, reqStart);
-  		break;
-  	case 1: cout << "One or more of the requested seats is unavailable. Please view the available seats and try again.\n";
-  		break;
-  }
+	switch (tm.requestTickets(reqSeats, reqRow, reqStart)) {
+	case 0: purchase(tm, reqSeats, reqRow, reqStart);
+		break;
+	case 1: cout << "One or more of the requested seats is unavailable. Please view the available seats and try again.\n";
+		break;
+	}
 }
 
 void purchase(TicketMaster &tm, int seats, int row, int start) {
@@ -102,19 +109,23 @@ void purchase(TicketMaster &tm, int seats, int row, int start) {
 	char buy = ' ';
 
 	cout << "Congratulations, those seats are available! You have requested " <<
-		seats << " seats. These seats are $" << fixed << setprecision(2)  << price << " each.\nThis brings your total to $" << total << ". Would you like to purchase these tickets? (Y or N) ";
+		seats << " seats. These seats are $" << fixed << setprecision(2) << price << " each.\nThis brings your total to $" << total << ". Would you like to purchase these tickets? (Y or N) ";
 	while (true) {
 		cin >> buy;
 		if (buy != 'y' && buy != 'Y' && buy != 'n' && buy != 'N') {
 			cout << "Invalid answer, please press Y if you would like to buy these tickets or N if you do not want to buy these tickets.\n";
-		} else if (buy == 'y' || buy == 'Y'){
+		}
+		else if (buy == 'y' || buy == 'Y') {
 			cout << "Great!\n";
 			cout << tm.purchaseTickets(seats, row, start, total);
 			break;
-		} else {
+		}
+		else {
 			cout << "Returning to main menu.\n\n";
 			break;
 		}
 	}
+ 	
 	cout << "Seats sold: " << tm.getSeatsSold() << " Total Money: " << tm.getTotalMoney() << endl;
+
 }
